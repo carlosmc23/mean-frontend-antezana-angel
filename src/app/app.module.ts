40c79f544820894077d08b2165212c3f62a68020
carlos <kenshinmc23@gmail.com>
2018-03-27 20:56:37 -0400

    Setup material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './material/material.module';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
@@ -34,6 +36,7 @@ import { UserInformationComponent } from './user-information/user-information.co
   ],
   imports: [
     BrowserModule,
+    MaterialModule,
     FormsModule,
     AppRoutingModule,
     HttpClientModule
